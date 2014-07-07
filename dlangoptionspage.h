b9472c9efd84cd0ef86d64afa92e5d0f0883a981
Cleem <cleemmi@gmail.com>
2014-07-07 11:23:52 +0400

    Options page fix *Previously uncommited

--- dlangoptionspage.h
+++ dlangoptionspage.h
@@ -1,10 +1,51 @@
 #ifndef DLANGOPTIONSPAGE_H
 #define DLANGOPTIONSPAGE_H
 
+#include <coreplugin/dialogs/ioptionspage.h>
+
+#include <QWidget>
+#include <QPointer>
+
+namespace Utils {
+class PathChooser;
+}
+
+namespace DlangEditor {
+
+class DlangOptionsPageWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    DlangOptionsPageWidget(QWidget *parent = 0);
+    virtual ~DlangOptionsPageWidget();
+    QString clientExecutable() const;
+    QString serverExecutable() const;
+    QString phobosPath() const;
+private:
+    Utils::PathChooser *m_client;
+    Utils::PathChooser *m_server;
+    Utils::PathChooser *m_phobos;
+};
+
 class DlangOptionsPage : public Core::IOptionsPage
 {
 public:
     DlangOptionsPage();
+    virtual ~DlangOptionsPage();
+
+    // pure Core::IOptionsPage
+    virtual QWidget *widget();
+    virtual void apply();
+    virtual void finish();
+
+    // others
+    static QString dcdClientExecutable();
+    static QString dcdServerExecutable();
+    static QString phobosDir();
+private:
+    QPointer<DlangOptionsPageWidget> m_widget;
 };
+
+}
 
 #endif // DLANGOPTIONSPAGE_H
