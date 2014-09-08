c5b99ed651d432bf1248dccb840cb66bf738b352
Cleem <cleemmi@gmail.com>
2014-09-09 00:07:56 +0400

    Decorate tooltip +Simple documentation tooltip decoration

--- dlanghoverhandler.cpp
+++ dlanghoverhandler.cpp
@@ -67,7 +67,36 @@ void DlangHoverHandler::identifyMatch(TextEditor::ITextEditor *editor, int pos)
     setToolTip(lastTooltip);
 }
 
+static const QString DLANG_DOC_META = QLatin1String("$(D");
+
 void DlangHoverHandler::decorateToolTip()
 {
-
+    QString tip = toolTip();
+    if (tip.length() > 1000) {
+        tip.resize(1000);
+        tip.append("...");
+    }
+    int pos = 0;
+    while ((pos = tip.indexOf(DLANG_DOC_META, pos)) != -1) {
+        int balance = 1;
+        for (int end = pos + DLANG_DOC_META.length();  end < tip.length(); ++end) {
+            if (tip.at(end) == QLatin1Char('(')) {
+                ++balance;
+            } else if (tip.at(end) == QLatin1Char(')')) {
+                --balance;
+                if (balance == 0) {
+                    tip.replace(end, 1, QLatin1String("</b>"));
+                    tip.replace(pos, DLANG_DOC_META.length(), QLatin1String("<b>"));
+                    break;
+                }
+            }
+        }
+        if (balance > 0) {
+            tip.replace(pos, DLANG_DOC_META.length(), QLatin1String("<b>"));
+            tip.append(QLatin1String("</b>"));
+        }
+        ++pos;
+    }
+    tip.replace(QLatin1String("\\n"), QLatin1String("\n"));
+    setToolTip(tip);
 }
